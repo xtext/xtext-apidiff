5228987e635601e85c6932ca2aac08e7db5c09bb
Christian Dietrich <christian.dietrich@itemis.de>
2019-07-05 10:15:11 +0200

    added bash script to create report
    Signed-off-by: Christian Dietrich <christian.dietrich@itemis.de>

---
+++ create-api-diff.sh
@@ -0,0 +1,92 @@
+#!/bin/bash
+
+cd xtext-apidiff
+./gradlew clean build
+cd ..
+
+if [ -z "$NEW_VERSION" ]; then
+  # if not set in environment use default
+  NEW_VERSION=2.19.0
+fi
+
+if [ -z "$OLD_VERSION" ]; then
+  # if not set in environment use default
+  OLD_VERSION=2.18.0
+fi
+
+echo "Diffing $NEW_VERSION against $OLD_VERSION"
+
+if [ -f japicmp-ext.jar ];
+then
+  rm japicmp-ext.jar
+fi
+cp xtext-apidiff/build/libs/japicmp-ext.jar .
+
+if [ ! -d eclipse ];
+then
+   echo "Downloading Eclipse"
+   # Get basic Eclipse SDK
+   wget -q 'http://download.eclipse.org/eclipse/downloads/drops4/R-4.12-201906051800/eclipse-SDK-4.12-linux-gtk-x86_64.tar.gz'
+   tar -zxf eclipse-SDK-4.12-linux-gtk-x86_64.tar.gz
+   rm eclipse-SDK-4.12-linux-gtk-x86_64.tar.gz
+   # Install additional features: Xtext and dependent
+   eclipse/eclipse -data eclipse/.director-ws -consolelog -noSplash -clean \
+   -application org.eclipse.equinox.p2.director \
+   -metadataRepository http://download.eclipse.org/modeling/tmf/xtext/updates/releases/2.18.0/,http://download.eclipse.org/releases/photon,http://download.eclipse.org/lsp4j/updates/releases/ \
+   -artifactRepository http://download.eclipse.org/modeling/tmf/xtext/updates/releases/2.18.0/,http://download.eclipse.org/releases/photon,http://download.eclipse.org/lsp4j/updates/releases/ \
+   -installIU org.eclipse.xtext.sdk.feature.group,org.eclipse.lsp4j.sdk.feature.group,org.eclipse.m2e.core,org.kohsuke.args4j \
+   -destination eclipse
+fi
+
+if [ ! -d tmf-xtext-Update-2.17.0 ];
+then
+   echo "Downloading Xtext 2.17.0"
+   wget -q 'download.eclipse.org/modeling/tmf/xtext/downloads/drops/2.17.0/R201903041445/tmf-xtext-Update-2.17.0.zip'
+   unzip -q tmf-xtext-Update-2.17.0.zip -d tmf-xtext-Update-2.17.0
+   rm tmf-xtext-Update-2.17.0.zip
+fi
+
+if [ ! -d tmf-xtext-Update-2.17.1 ];
+then
+   echo "Downloading Xtext 2.17.1"
+   wget -q 'download.eclipse.org/modeling/tmf/xtext/downloads/drops/2.17.1/R201904030733/tmf-xtext-Update-2.17.1.zip'
+   unzip -q tmf-xtext-Update-2.17.1.zip -d tmf-xtext-Update-2.17.1
+   rm tmf-xtext-Update-2.17.1.zip
+fi
+
+if [ ! -d tmf-xtext-Update-2.18.0 ];
+then
+   echo "Downloading Xtext 2.18.0"
+   wget -q 'download.eclipse.org/modeling/tmf/xtext/downloads/drops/2.18.0/R201906031516/tmf-xtext-Update-2.18.0.zip'
+   unzip -q tmf-xtext-Update-2.18.0.zip -d tmf-xtext-Update-2.18.0
+   rm tmf-xtext-Update-2.18.0.zip
+fi
+
+if [ -d tmf-xtext-Update-2.19.0 ];
+then
+  rm -r tmf-xtext-Update-2.19.0
+fi
+echo "Downloading Xtext 2.19.0"
+# TODO make this better and faster. am not sure if we can guess the nightly number. or ....
+wget -q --output-document=tmf-xtext-Update-2.19.0.zip 'https://ci-staging.eclipse.org/xtext/job/releng/job/sign-and-deploy/lastSuccessfulBuild/artifact/build-result/p2.repository/*zip*/p2.repository.zip' 
+unzip -q tmf-xtext-Update-2.19.0.zip
+# i really dislike this
+mv p2.repository tmf-xtext-Update-2.19.0
+rm tmf-xtext-Update-2.19.0.zip
+
+# update apicmp.properties
+{
+	echo "old.version=${OLD_VERSION}"
+	echo "new.version=${NEW_VERSION}"
+	echo "old.location=tmf-xtext-Update-${OLD_VERSION}/plugins/"
+	echo "new.location=tmf-xtext-Update-${NEW_VERSION}/plugins/"
+	echo "cpLocation=eclipse/plugins"
+
+} > japicmp.properties
+
+if [ -d output ];
+then
+  rm -r output
+fi
+echo "creating the actual diff"
+java -jar japicmp-ext.jar
