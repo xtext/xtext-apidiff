109b3a3b01b89bda57fdfd3672e2f1923ed9df31
Christian Dietrich <christian.dietrich@itemis.de>
2019-08-23 07:32:33 +0200

    Added orbit to update sites
    Signed-off-by: Christian Dietrich <christian.dietrich@itemis.de>

--- create-api-diff.sh
+++ create-api-diff.sh
@@ -31,8 +31,8 @@ then
    # Install additional features: Xtext and dependent
    eclipse/eclipse -data eclipse/.director-ws -consolelog -noSplash -clean \
    -application org.eclipse.equinox.p2.director \
-   -metadataRepository http://download.eclipse.org/modeling/tmf/xtext/updates/releases/2.18.0/,http://download.eclipse.org/releases/2019-06,http://download.eclipse.org/lsp4j/updates/releases/ \
-   -artifactRepository http://download.eclipse.org/modeling/tmf/xtext/updates/releases/2.18.0/,http://download.eclipse.org/releases/2019-06,http://download.eclipse.org/lsp4j/updates/releases/ \
+   -metadataRepository http://download.eclipse.org/modeling/tmf/xtext/updates/releases/2.18.0/,http://download.eclipse.org/releases/2019-06,http://download.eclipse.org/lsp4j/updates/releases/,https://download.eclipse.org/tools/orbit/downloads/2019-09 \
+   -artifactRepository http://download.eclipse.org/modeling/tmf/xtext/updates/releases/2.18.0/,http://download.eclipse.org/releases/2019-06,http://download.eclipse.org/lsp4j/updates/releases/,https://download.eclipse.org/tools/orbit/downloads/2019-09 \
    -installIU org.eclipse.xtext.sdk.feature.group,org.eclipse.lsp4j.sdk.feature.group,org.eclipse.m2e.core,org.kohsuke.args4j \
    -destination eclipse
 fi
